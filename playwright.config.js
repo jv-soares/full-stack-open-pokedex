21a94fb37d86f5d3b575bb905d7efa2d73261939
João Vitor <j.soares@aparkado.de>
2025-03-22 21:09:05 +0100

    fix playwright web server port

--- playwright.config.js
+++ playwright.config.js
@@ -74,7 +74,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
-    url: 'http://localhost:8080',
+    url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
   },
 })
